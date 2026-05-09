1131856ab059e815545292778616751fdd6aa7f7
shj366 <10422087@qq.com>
2026-05-09 17:33:43 +0800

    fix: 修改工作流初始化sql

--- sql/mysql/init.sql
+++ sql/mysql/init.sql
@@ -16,10 +16,6 @@ set @workflow_center_id = (select id from sys_menu where name = 'WorkflowCenter'
 set @workflow_apply_id = (select id from sys_menu where name = 'WorkflowApply' and parent_id = @workflow_menu_id);
 set @process_design_id = (select id from sys_menu where name = 'WorkflowProcessDesign' and parent_id = @workflow_menu_id);
 set @process_define_id = (select id from sys_menu where name = 'WorkflowProcessDefine' and parent_id = @workflow_menu_id);
-set @task_todo_id = (select id from sys_menu where name = 'WorkflowTaskTodo' and parent_id = @workflow_menu_id);
-set @task_done_id = (select id from sys_menu where name = 'WorkflowTaskDone' and parent_id = @workflow_menu_id);
-set @instance_my_id = (select id from sys_menu where name = 'WorkflowInstanceMy' and parent_id = @workflow_menu_id);
-set @instance_cc_id = (select id from sys_menu where name = 'WorkflowInstanceCc' and parent_id = @workflow_menu_id);
 
 -- 流程设计权限
 insert into sys_menu (title, name, path, sort, icon, type, component, perms, status, display, cache, link, remark, parent_id, created_time, updated_time)
@@ -45,20 +41,20 @@ values
 -- 任务权限
 insert into sys_menu (title, name, path, sort, icon, type, component, perms, status, display, cache, link, remark, parent_id, created_time, updated_time)
 values
-('查看待办任务', 'ViewWorkflowTodoTask', null, 0, null, 2, null, 'workflow:task:todo:view', 1, 0, 1, '', null, @task_todo_id, now(), null),
-('查看已办任务', 'ViewWorkflowDoneTask', null, 0, null, 2, null, 'workflow:task:done:view', 1, 0, 1, '', null, @task_done_id, now(), null),
-('完成任务', 'CompleteWorkflowTask', null, 0, null, 2, null, 'workflow:task:complete', 1, 0, 1, '', null, @task_todo_id, now(), null),
-('驳回任务', 'RejectWorkflowTask', null, 0, null, 2, null, 'workflow:task:reject', 1, 0, 1, '', null, @task_todo_id, now(), null),
-('退回任务', 'RollbackWorkflowTask', null, 0, null, 2, null, 'workflow:task:rollback', 1, 0, 1, '', null, @task_todo_id, now(), null),
-('跳转节点', 'JumpWorkflowTask', null, 0, null, 2, null, 'workflow:task:jump', 1, 0, 1, '', null, @task_todo_id, now(), null),
-('加签任务', 'AddCandidateWorkflowTask', null, 0, null, 2, null, 'workflow:task:add-candidate', 1, 0, 1, '', null, @task_todo_id, now(), null),
-('委托任务', 'SurrogateWorkflowTask', null, 0, null, 2, null, 'workflow:task:surrogate', 1, 0, 1, '', null, @task_todo_id, now(), null),
-('抄送任务', 'CcWorkflowTask', null, 0, null, 2, null, 'workflow:task:cc', 1, 0, 1, '', null, @task_todo_id, now(), null);
+('查看待办任务', 'ViewWorkflowTodoTask', null, 0, null, 2, null, 'workflow:task:todo:view', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('查看已办任务', 'ViewWorkflowDoneTask', null, 0, null, 2, null, 'workflow:task:done:view', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('完成任务', 'CompleteWorkflowTask', null, 0, null, 2, null, 'workflow:task:complete', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('驳回任务', 'RejectWorkflowTask', null, 0, null, 2, null, 'workflow:task:reject', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('退回任务', 'RollbackWorkflowTask', null, 0, null, 2, null, 'workflow:task:rollback', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('跳转节点', 'JumpWorkflowTask', null, 0, null, 2, null, 'workflow:task:jump', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('加签任务', 'AddCandidateWorkflowTask', null, 0, null, 2, null, 'workflow:task:add-candidate', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('委托任务', 'SurrogateWorkflowTask', null, 0, null, 2, null, 'workflow:task:surrogate', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('抄送任务', 'CcWorkflowTask', null, 0, null, 2, null, 'workflow:task:cc', 1, 0, 1, '', null, @workflow_center_id, now(), null);
 
 -- 流程实例权限
 insert into sys_menu (title, name, path, sort, icon, type, component, perms, status, display, cache, link, remark, parent_id, created_time, updated_time)
 values
-('查看我的流程', 'ViewWorkflowInstanceMy', null, 0, null, 2, null, 'workflow:instance:my:view', 1, 0, 1, '', null, @instance_my_id, now(), null),
-('撤回我的流程', 'WithdrawWorkflowInstanceMy', null, 0, null, 2, null, 'workflow:instance:my:withdraw', 1, 0, 1, '', null, @instance_my_id, now(), null),
-('查看抄送', 'ViewWorkflowInstanceCc', null, 0, null, 2, null, 'workflow:instance:cc:view', 1, 0, 1, '', null, @instance_cc_id, now(), null),
-('标记抄送已读', 'ReadWorkflowInstanceCc', null, 0, null, 2, null, 'workflow:instance:cc:read', 1, 0, 1, '', null, @instance_cc_id, now(), null);
+('查看我的流程', 'ViewWorkflowInstanceMy', null, 0, null, 2, null, 'workflow:instance:my:view', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('撤回我的流程', 'WithdrawWorkflowInstanceMy', null, 0, null, 2, null, 'workflow:instance:my:withdraw', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('查看抄送', 'ViewWorkflowInstanceCc', null, 0, null, 2, null, 'workflow:instance:cc:view', 1, 0, 1, '', null, @workflow_center_id, now(), null),
+('标记抄送已读', 'ReadWorkflowInstanceCc', null, 0, null, 2, null, 'workflow:instance:cc:read', 1, 0, 1, '', null, @workflow_center_id, now(), null);
