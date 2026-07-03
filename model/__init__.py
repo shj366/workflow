6b881278ee1a5704e8ddf58490f821e1077ad6d6
shj366 <10422087@qq.com>
2026-07-03 18:11:59 +0800

    fix: export workflow models

---
+++ model/__init__.py
@@ -0,0 +1,6 @@
+from backend.plugin.wf.model.process_cc_instance import ProcessCCInstance as ProcessCCInstance
+from backend.plugin.wf.model.process_define import ProcessDefine as ProcessDefine
+from backend.plugin.wf.model.process_design import ProcessDesign as ProcessDesign
+from backend.plugin.wf.model.process_instance import ProcessInstance as ProcessInstance
+from backend.plugin.wf.model.process_task import ProcessTask as ProcessTask
+from backend.plugin.wf.model.process_task_actor import ProcessTaskActor as ProcessTaskActor
